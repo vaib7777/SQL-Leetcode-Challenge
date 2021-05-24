1cabd77f3c2b7d61d282406c15e4a10ee3a8bc83
Vaibhav Bijola <vaibhavbijola@gmail.com>
2021-05-23 21:42:29 -0500

    Update 550 - Game Play Analysis IV.sql

--- Medium/550 - Game Play Analysis IV.sql
+++ Medium/550 - Game Play Analysis IV.sql
@@ -13,7 +13,8 @@ This table shows the activity of players of some game.
 Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
  
 
-Write an SQL query that reports the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+Write an SQL query that reports the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. 
+In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
 
 The query result format is in the following example:
 
